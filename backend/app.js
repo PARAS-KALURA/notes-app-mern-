5cc3ea19de84d1ef0e7f354d6d3224ac8a809fa2
Paras Kalura <officialkalura@gmail.com>
2026-03-06 15:57:19 +0530

    enable cors for frontend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const noteRoutes = require("./routes/noteRoutes");
 
 const app = express();
 
+// enable CORS
+app.use(cors({
+  origin: "https://notes-app-mern-seven.vercel.app"
+}));
+
 app.use(express.json());
 
-// VERY IMPORTANT
+// routes
 app.use("/api/notes", noteRoutes);
 
 module.exports = app;
