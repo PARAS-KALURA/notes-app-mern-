963b21d2863bfbe517d839a8db708d7a1c11c6c4
Paras Kalura <officialkalura@gmail.com>
2026-03-06 15:51:59 +0530

    updated API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "http://localhost:3000/api/notes";
+const API = "https://notes-app-mern-rset.onrender.com/api/notes";
 
 function App() {
 
